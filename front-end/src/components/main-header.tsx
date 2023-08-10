afd088f3a01706f4ebfaf2880e356dd290221d66
saerom <saerom216@gamil.com>
2023-08-10 16:31:41 +0900

    [feat] Main Header 홈 아이콘 추가

--- front-end/src/components/main-header.tsx
+++ front-end/src/components/main-header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import {siteConfig} from '@/config/site';
 import {cn} from '@/lib/shadcn-ui/utils';
 import {SiTistory, SiNaver} from 'react-icons/si';
+import {BiHome} from 'react-icons/bi';
 import {MainNav} from '@/components/main-nav';
 import {ModeToggle} from '@/components/mode-toggle';
 import {buttonVariants} from '@/components/ui/button';
@@ -11,13 +12,28 @@ export function MainHeader() {
   return (
     <header className='supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur'>
       <div className='container flex h-14 items-center'>
+        <div className='items-center justify-between space-x-2'>
+          <Link href='/'>
+            <div
+              className={cn(
+                buttonVariants({
+                  variant: 'ghost',
+                }),
+                'w-12 p-0',
+              )}
+            >
+              <BiHome className='h-7 w-7' />
+              <span className='sr-only'>Home</span>
+            </div>
+          </Link>
+        </div>
         <MainNav />
         <div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
           <nav className='flex items-center'>
             <Link
               href={siteConfig.links.tistory}
               target='_blank'
-              rel='noreferrer'
+              rel='noopener noreferrer'
             >
               <div
                 className={cn(
@@ -34,7 +50,7 @@ export function MainHeader() {
             <Link
               href={siteConfig.links.naver}
               target='_blank'
-              rel='noreferrer'
+              rel='noopener noreferrer'
             >
               <div
                 className={cn(
