9a460c648ab383127ba8353b37d9f84b9d589bdd
saerom <saerom216@gamil.com>
2023-08-30 21:55:32 +0900

    [feat] AboutMe프로젝트 경험 추가

--- front-end/src/app/aboutMe/page.tsx
+++ front-end/src/app/aboutMe/page.tsx
@@ -30,6 +30,106 @@ const skillsList: {title: string; content: string}[] = [
   },
 ];
 
+const projectList: {
+  name: string;
+  company: string;
+  skills: string;
+  contents: string[];
+}[] = [
+  {
+    name: '물류로봇관제 웹 (2022-10 ~ 2023-02)',
+    company: '(주)나비프라',
+    skills:
+      'Vue, JavaScript, TypeScript, Vuex, Pinia, Vuetify, three.js, Node.js, express.js, Redis, PostgreSQL, Docker',
+    contents: [
+      '로봇이 Mapping한 지도를 뿌려주고 로봇의 위치와 경로를 설정할 수 있는 물류 로봇 관제 웹 어플리케이션 개발',
+      'Front-end 및 Back-end 메인 개발',
+    ],
+  },
+  {
+    name: '현대차 물류로봇관제 웹 (2022-06)',
+    company: '(주)나비프라',
+    skills: 'React, JavaScript, Vuetify, three.js, Node.js, express.js, Redis',
+    contents: [
+      '물류 로봇을 관제하는 웹 어플리케이션 개발',
+      'Front-end 및 Back-end 개발',
+    ],
+  },
+  {
+    name: '웹용 품질관리 솔루션 (2022-02 ~ 2022-07)',
+    company: '(주)에스이랩',
+    skills: 'React, TypeScript, Next.js, Redux, Material UI',
+    contents: [
+      '제조 공장의 품질 향상을 위한 데이터 분석/가공/시각화 솔루션으로 윈도우용 어플리케이션을 웹 어플리케이션으로 마이그레이션',
+      'Front-end 메인 개발',
+    ],
+  },
+  {
+    name: '윈도우즈용 품질관리 솔루션 (2019.02 ~ 2021.04)',
+    company: '(주)에스이랩',
+    skills: 'C#, XMAL, WPF, HTML/CSS, Docker, Jenkins, swagger',
+    contents: [
+      '제조 공장의 품질 향상을 위한 데이터 분석/가공/시각화 솔루션으로 윈도우즈용 프로그램 기획/운영/개발',
+      'Front-end, Back-end 메인 개발',
+    ],
+  },
+  {
+    name: '기상청 홈페이지 통합 정비 사업 (2021.04 ~ 2021.04)',
+    company: '(주)에스이랩',
+    skills: 'Java, JavaScript, HTML/CSS, Tomcat',
+    contents: [
+      '기상청 지방청 및 지청 홈페이지 개선',
+      'Front-end, Back-end 개발',
+    ],
+  },
+  {
+    name: '기상청 이중편파레이더 현업예보지원 (2018.06 ~ 2020.11)',
+    company: '(주)에스이랩',
+    skills:
+      'Java, JavaScript, HTML/CSS, Spring, Oracle Database, Tomcat, React Native',
+    contents: [
+      '기상청 레이더 정보 및 날씨 경보를 표출하는 웹 및 안드로이드/iOS 앱 개발',
+      'Front-end, Back-end 메인 개발',
+    ],
+  },
+  {
+    name: '수자원공사 위성산출물 표출 시스템 (2018.12 ~ 2019.02)',
+    company: '(주)에스이랩',
+    skills: 'Java, JavaScript, HTML/CSS, Spring, MySQL, Tomcat',
+    contents: [
+      '위성 지도 위에 위성산출물을 표출하는 GIS 기반 웹 사이트 개발',
+      'Front-end, Back-end 메인 개발',
+    ],
+  },
+  {
+    name: '기상청 국가기후자료관리 및 서비스체계 구축 (2018.02 ~ 2018.05)',
+    company: '(주)에스이랩',
+    skills: 'Java, JavaScript, HTML/CSS, Spring, Oracle Database, Tomcat',
+    contents: [
+      '기상청 기상기후 데이터를 다양한 포맷으로 다운로드/열람하는 기상자료개방포털 개발',
+      'Front-end, Back-end 개발',
+    ],
+  },
+  {
+    name: '우주전파센터 우주환경 예보 통합모델 개발 (2018-02 ~ 2018-04)',
+    company: '(주)에스이랩',
+    skills: 'Java, JavaScript, HTML/CSS, Spring, MySQL, Tomcat',
+    contents: [
+      '우주환경 예보 분석 결과를 표출하는 웹 어플리케이션으로 표출 속도 110% 개선 및 컨텐츠 추가 개발',
+      'Front-end, Back-end 개발',
+    ],
+  },
+  {
+    name: '서울시 건축물 내진성능 자가점검 시스템 웹 개발 (2018.01 ~ 2019.01)',
+    company: '(주)에스이랩',
+    skills: 'Java, JavaScript, HTML/CSS, Spring, Oracle Database, JEUS',
+    contents: [
+      '건물 내진성능 자가점검 시스템과 지진 정보 제공 대국민 웹 개발 및 운영',
+      'Front-end, Back-end 개발',
+    ],
+  },
+];
+
 export default function AboutMePage() {
   return (
     <div className='container relative flex h-full w-3/4 flex-col'>
@@ -55,7 +155,7 @@ export default function AboutMePage() {
           </div>
         </section>
         <section className='my-10 text-sm lg:text-base'>
-          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-2xl'>
+          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-3xl'>
             소개
           </h1>
           <p>
@@ -111,7 +211,7 @@ export default function AboutMePage() {
           </p>
         </section>
         <section className='my-10 text-sm lg:text-base'>
-          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-2xl'>
+          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-3xl'>
             보유 기술
           </h1>
           {skillsList.map((skill) => (
@@ -121,13 +221,22 @@ export default function AboutMePage() {
           ))}
         </section>
         <section className='my-10 text-sm lg:text-base'>
-          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-2xl'>
-            프로젝트 경력
+          <h1 className='my-3 scroll-m-20 border-b pb-2 text-xl font-bold tracking-tight lg:text-3xl'>
+            프로젝트 경험
           </h1>
-          {skillsList.map((skill) => (
-            <p className='[&:not(:first-child)]:mb-2'>
-              <span className='font-bold'>{skill.title}</span>: {skill.content}
-            </p>
+          {projectList.map((project, index) => (
+            <div className='mb-10' key={index}>
+              <h2 className='mb-1 text-lg font-bold lg:text-xl'>
+                {project.name}
+              </h2>
+              <p className='my-1 font-semibold'>{project.company}</p>
+              <p className='font-semibold'>{project.skills}</p>
+              <ul className='mb-6 ml-6 list-disc [&>li]:mt-2'>
+                {project.contents.map((content, index) => (
+                  <li key={index}>{content}</li>
+                ))}
+              </ul>
+            </div>
           ))}
         </section>
       </article>
