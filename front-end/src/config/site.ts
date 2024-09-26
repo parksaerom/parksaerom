9e6aa6d118fe5834012109c4f38d34b25b01b763
saerom <saerom216@gamil.com>
2024-09-26 16:23:38 +0900

    [front-end] [config] metadata description 수정

--- front-end/src/config/site.ts
+++ front-end/src/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: 'parksaerom.vercel.app',
   creator: 'parksaerom',
   email: 'saerom216@gmail.com',
-  description: '개발자 박새롬의 포트폴리오 웹 사이트에 오신 것을 환영합니다.',
+  description: '웹 개발자 박새롬의 포트폴리오 사이트입니다.',
   links: {
     tistory: 'https://saeromee.tistory.com/',
     naver: 'https://blog.naver.com/saerom_95',
